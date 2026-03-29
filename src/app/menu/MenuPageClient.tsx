1655603f4ab31d479828014d942b61c7bc701a6c
Raphaël Barman <rbarman@hotmail.ch>
2026-03-29 17:49:01 +0200

    Remove invalid <menu-traiteurSection/> tag causing build failure

--- src/app/menu/MenuPageClient.tsx
+++ src/app/menu/MenuPageClient.tsx
@@ -131,7 +131,6 @@ export function MenuPageClient({ sections }: MenuPageClientProps) {
                     </div>
                 </div>
             </section>
-            <menu-traiteurSection/>
             <ReserveSection />
         </div>
     );
